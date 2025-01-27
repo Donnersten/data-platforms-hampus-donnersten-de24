ae136e08cc8054ba970b9481c9aead31d41a2be0
Donnersten <hampus@donnersten.se>
2025-01-27 13:10:09 +0100

    kafka producer code along

---
+++ code_along/08_producer_consumer/src/producer.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import json
+from pprint import pprint
+from quixstreams import Application
+
+# .parents[1] = [1] = hamna i 08_producer_costumer [0] = src mappen
+data_path = Path(__file__).parents[1] / "data"
+#print(data_path)
+
+with open(data_path / "jokes.json", "r") as file:
+    jokes = json.load(file)
+
+#pprint(jokes)
+
+app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
+
+jokes_topic = app.topic(name= "jokes", value_serializer="json")
+
+#print(jokes_topic)
+
+def main():
+    with app.get_producer() as producer:
+        #print(producer)
+
+        for joke in jokes:
+            kafka_meg = jokes_topic.serialize(key=joke["joke_id"], value=joke)
+
+            print(f"Preduced  message: key = {kafka_meg.key} value = {kafka_meg.value}")
+
+            producer.produce(
+                topic="jokes", key= str(kafka_meg.key), value=kafka_meg.value
+            )
+            
+
+if __name__ == "__main__":
+    #pprint(jokes)
+    main()

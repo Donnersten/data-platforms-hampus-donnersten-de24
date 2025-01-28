9a1c05f14a1d569fb1864d261d79ccc72fdf0af0
Donnersten <hampus@donnersten.se>
2025-01-28 09:39:25 +0100

    added comment

--- code_along/08_producer_consumer/src/producer.py
+++ code_along/08_producer_consumer/src/producer.py
@@ -7,11 +7,13 @@ from quixstreams import Application
 data_path = Path(__file__).parents[1] / "data"
 #print(data_path)
 
+# Läser in json filen
 with open(data_path / "jokes.json", "r") as file:
     jokes = json.load(file)
 
 #pprint(jokes)
 
+# En form av öppning av kafka, localhost:9092 är som en port mot broker container 
 app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
 
 jokes_topic = app.topic(name= "jokes", value_serializer="json")

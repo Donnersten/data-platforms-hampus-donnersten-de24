98ba24998e5b6aec22a8d2e1575217b2040a5d60
Donnersten <hampus@donnersten.se>
2025-02-14 10:25:32 +0100

    13 code along done

---
+++ code_along/13_live_dashboard/src/dashboard.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from constants import (
+    POSTGRES_DBNAME,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+)
+from sqlalchemy import create_engine
+import pandas as pd
+from charts import line_chart
+from streamlit_autorefresh import st_autorefresh
+
+conection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
+
+engine = create_engine(conection_string)
+
+count = st_autorefresh(interval=1000*10, limit=100)
+
+def load_data(query):
+    with engine.connect() as conn:
+        df = pd.read_sql(query, conn)
+    return df
+
+
+def layout():
+    df = load_data("SELECT * FROM bitcoin;")
+
+    st.markdown("# Bitcoin data")
+
+    st.markdown("## Latest data ")
+
+    st.markdown("Latest data directly from postgres database in a docker container")
+
+    st.dataframe(df.tail())
+
+    st.markdown("## Bitcoin latest price in USD")
+
+    # plotting code
+    price_chart = line_chart(
+        df["timestamp"],
+        df["price_usd"],
+        title="price USD for bitcoin",
+    )
+
+    st.pyplot(price_chart)
+
+if __name__ == "__main__":
+    layout()
